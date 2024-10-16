713616dffe72650a5839e0d2e3ac17fcdac3ca39
Etern <makinoharataka@gmail.com>
2024-10-16 15:11:52 +0100

    This a new NoteApi class with controllers package

---
+++ src/main/kotlin/controllers/NoteApi.kt
@@ -0,0 +1,36 @@
+package ie.setu.controllers
+
+import ie.setu.models.Note
+
+class NoteApi {
+    private var notes = ArrayList<Note>()
+    fun add(note: Note): Boolean {
+        return notes.add(note)
+    }
+    fun listAllNotes(): String {
+        return if (notes.isEmpty()) {
+            "No notes stored"
+        } else {
+            var listOfNotes = ""
+            for (i in notes.indices) {
+                listOfNotes += "${i}: ${notes[i]} \n"
+            }
+            listOfNotes
+        }
+    }
+    fun numberOfNotes(): Int {
+        return notes.size
+    }
+
+    fun findNote(index: Int): Note? {
+        return if (isValidListIndex(index, notes)) {
+            notes[index]
+        } else null
+    }
+
+    //utility method to determine if an index is valid in a list.
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
+
+}
---
+++ src/main/kotlin/models/Note.kt
@@ -0,0 +1,10 @@
+package ie.setu.models
+
+
+data class Note(val noteTitle: String,
+                val notePriority: Int,
+                val noteCategory: String,
+                val isNoteArchived :Boolean){
+}
+
+
